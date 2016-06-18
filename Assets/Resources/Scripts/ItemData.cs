d4ed35dff175c05fdc12d48ecf798ad026e4978c
PeachTreeOath <peachtreeoath@gmail.com>
2016-06-18 01:37:34 -0600

    Item data

---
+++ Assets/Resources/Scripts/ItemData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+public class ItemData : MonoBehaviour
+{
+
+    private string jsonString;
+    private JsonData itemData;
+
+    // Use this for initialization
+    void Start()
+    {
+        jsonString = File.ReadAllText(Application.dataPath + "/Resources/Scripts/Items.json");
+        itemData = JsonMapper.ToObject(jsonString);
+
+        Debug.Log(itemData["Body"][0]["name"]);
+    }
+
+    JsonData GetItem(string name, string type)
+    {
+        for (int i = 0; i < itemData[type].Count; i++)
+        {
+            if (itemData[type][i]["name"].ToString() == name)
+            {
+                return itemData[type][i];
+            }
+        }
+
+        return null;
+    }
+}
